81577e84f09cfa6768a9d53c730d14d98b319263
Bwolf <bwolfnoob@gmail.com>
2022-01-20 21:41:20 -0300

    initial

---
+++ lib/movie_app_core.dart
@@ -0,0 +1,8 @@
+library movie_app_core;
+
+export 'src/analytics/coffsy_analytics.dart';
+export 'src/cache/shared_pref_helper.dart';
+export 'src/crashalytics/crashalytics_services.dart';
+export 'src/either_adapter/dartz_either_adapter.dart';
+export 'src/error/failure.dart';
+export 'src/network/network.dart';
